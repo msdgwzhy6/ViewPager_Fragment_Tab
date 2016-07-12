2bc8241381a6e3d1eeca3d4afa92860e077995aa
zuiwuyuan <1936900253@qq.com>
2016-07-12 10:56:42 +0800

    使用ViewPager和ragment实现顶部导航栏效果

---
+++ viewpager_fragment_tab/src/main/java/com/example/viewpagerdemo/FragmentAdapter.java
@@ -0,0 +1,29 @@
+package com.example.viewpagerdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    List<Fragment> fragmentList = new ArrayList<Fragment>();
+
+    public FragmentAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+}
